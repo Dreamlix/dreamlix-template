4d530b9e3278e76146f89e36597bd77e5f4d8a92
Ruan Haarhoff <ruan.haarhoff@gmail.com>
2021-02-21 17:41:33 +0000

    Added transition components.

---
+++ src/core/components/transitions/SectionTransitionBottomSplash.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="container-bottom-transition">
+		<div class="image-block">
+			<img class="bottom-transition" src="/texture-bottom.png" />
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	name: 'SectionTransitionBottomSplash'
+});
+</script>
+
+<style scoped>
+.container-bottom-transition {
+	text-align: center;
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+}
+
+.image-block {
+	width: 60%;
+	height: 100px;
+	position: relative;
+}
+
+.bottom-transition {
+	width: 100%;
+	position: absolute;
+	bottom: 0;
+	left: 0;
+}
+</style>
---
+++ src/core/components/transitions/SectionTransitionTopSplash.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="container-transition-top">
+		<div class="img-block">
+			<img class="img-transition-top" src="/texture-top.png" />
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	name: 'SectionTransitionTopSplash'
+});
+</script>
+
+<style scoped>
+.container-transition-top {
+	text-align: center;
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+}
+
+.img-block {
+	width: 60%;
+	height: 100px;
+	position: relative;
+}
+
+.img-transition-top {
+	width: 100%;
+	position: absolute;
+	top: 0;
+	left: 0;
+}
+</style>
