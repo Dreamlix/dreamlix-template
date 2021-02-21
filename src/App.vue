2d893ae9b42551a6b85665fed01312337541bcd3
Angelo Melonas <angelomelonas@gmail.com>
2021-02-21 16:53:02 +0000

    Created and added AppBar (Header) component

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,22 @@
 <template>
 	<v-app>
-		<v-app-bar app color="grey lighten-3">
-			<div class="d-flex align-center">
-				<v-img
-					alt="Vuetify Logo"
-					class="shrink mr-2"
-					contain
-					src="https://images.squarespace-cdn.com/content/600fff8831539d708a565cc6/1611667752413-J16I28OW25P5CRTIA2OO/dreamcatcher.png?content-type=image%2Fpng"
-					transition="scale-transition"
-					width="40"
-				/>
-			</div>
-		</v-app-bar>
-
+		<app-bar logo="header-logo-white.png"></app-bar>
 		<v-main>
-			<landing-section-component
-					title="Provided Page Title"
-					subtitle="Provided Page SubTitle"
-			>
-				This content is filled via a default slot
-			</landing-section-component>`,
+			<landing-section-component title="Provided Page Title" subtitle="Provided Page SubTitle"> This content is filled via a default slot </landing-section-component>`,
 		</v-main>
+		<!-- <footer-alpha></footer-alpha> -->
 	</v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import LandingSectionComponent from '@/core/components/LandingSectionComponent.vue';
-import FooterAlpha from '@/core/templates/footers/FooterAlpha.vue';
+import AppBar from '@/core/components/AppBar.vue';
 
 export default Vue.extend({
 	components: {
-		LandingSectionComponent,
+		AppBar,
+		LandingSectionComponent
 		// FooterAlpha
 	},
 
@@ -42,13 +27,10 @@ export default Vue.extend({
 </script>
 
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap');
 
-	html, body {
-		font-family: 'Amatic SC';
-	}
-
-	#app {
-		font-family: 'Amatic SC';
-	}
+html,
+body {
+	font-family: 'Amatic SC', serif;
+}
 </style>
