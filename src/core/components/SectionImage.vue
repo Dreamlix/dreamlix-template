6debdfc7cc2c215f3e840103ba2223bc705499c6
Ruan Haarhoff <ruan.haarhoff@gmail.com>
2021-02-21 17:40:31 +0000

    Added image section.

---
+++ src/core/components/SectionImage.vue
@@ -0,0 +1,28 @@
+<template>
+	<v-parallax :src="backgroundImage" :style="{ 'background-color': backgroundColor }" :height="height">
+		<slot name="transition-top"></slot>
+		<slot name="transition-bottom"></slot>
+	</v-parallax>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	components: {},
+	props: {
+		backgroundImage: {
+			type: String,
+			default: ''
+		},
+		backgroundColor: {
+			type: String,
+			default: '#FFFFFF'
+		},
+		height: {
+			type: String,
+			default: '400'
+		}
+	}
+});
+</script>
