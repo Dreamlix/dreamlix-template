2bf989aba2b591ef1930dd4cb834158c458c4603
Ruan Haarhoff <ruan.haarhoff@gmail.com>
2021-02-21 17:53:24 +0000

    Updated app to include newly created components.

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,41 @@
 		</v-app-bar>
 
 		<v-main>
-			<landing-section-component
-					title="Provided Page Title"
-					subtitle="Provided Page SubTitle"
-			>
-				This content is filled via a default slot
-			</landing-section-component>`,
+			<landing-section-component title="Provided Page Title" background-image="https://i.imgur.com/bX2Uk7C.jpg">
+				<template v-slot:transition-top>
+					<component :is="transitionTop"></component>
+				</template>
+				<template v-slot:transition-bottom>
+					<component :is="transitionBottom"></component>
+				</template>
+			</landing-section-component>
+			<div style="height: 400px;"></div>
+			<section-image background-image="https://i.imgur.com/bX2Uk7C.jpg">
+				<template v-slot:transition-top>
+					<component :is="transitionTop"></component>
+				</template>
+				<template v-slot:transition-bottom>
+					<component :is="transitionBottom"></component>
+				</template>
+			</section-image>
+			<div style="height: 400px;">
+				<v-row>
+					<v-col cols="2"> </v-col>
+					<v-col cols="8">
+						<v-row>
+							<v-col cols="4">
+								<card-info-minimal title="Title" background-image="/card-accommodation.jpg" link=""> </card-info-minimal>
+							</v-col>
+							<v-col cols="4">
+								<card-info-minimal title="Accommodation" background-image="/card-accommodation.jpg" link=""> </card-info-minimal>
+							</v-col>
+							<v-col cols="4">
+								<card-info-minimal title="Title" background-image="/card-accommodation.jpg" link=""> </card-info-minimal>
+							</v-col>
+						</v-row>
+					</v-col>
+				</v-row>
+			</div>
 		</v-main>
 	</v-app>
 </template>
@@ -27,28 +56,36 @@
 <script lang="ts">
 import Vue from 'vue';
 import LandingSectionComponent from '@/core/components/LandingSectionComponent.vue';
-import FooterAlpha from '@/core/templates/footers/FooterAlpha.vue';
+import SectionImage from '@/core/components/SectionImage.vue';
+import SectionTransitionBottomSplash from '@/core/components/transitions/SectionTransitionBottomSplash.vue';
+import SectionTransitionTopSplash from '@/core/components/transitions/SectionTransitionTopSplash.vue';
+import CardInfoMinimal from '@/core/components/cards/CardInfoMinimal.vue';
 
 export default Vue.extend({
 	components: {
 		LandingSectionComponent,
-		// FooterAlpha
+		SectionTransitionTopSplash,
+		SectionTransitionBottomSplash,
+		SectionImage,
+		CardInfoMinimal
 	},
 
 	data: () => ({
-		//
+		transitionTop: 'SectionTransitionTopSplash',
+		transitionBottom: 'SectionTransitionBottomSplash'
 	})
 });
 </script>
 
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap');
 
-	html, body {
-		font-family: 'Amatic SC';
-	}
+html,
+body {
+	font-family: 'Amatic SC';
+}
 
-	#app {
-		font-family: 'Amatic SC';
-	}
+#app {
+	font-family: 'Amatic SC';
+}
 </style>
