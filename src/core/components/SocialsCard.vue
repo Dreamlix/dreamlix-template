dc57439e1db24e37727727a71bf4003858065294
Ruan Haarhoff <ruan.haarhoff@gmail.com>
2021-02-23 10:37:33 +0000

    Updated social cards to be more simple for now.

--- src/core/components/SocialsCard.vue
+++ src/core/components/SocialsCard.vue
@@ -1,23 +1,9 @@
 <template>
 	<v-card flat color="transparent">
-		<v-card-title class="pt-0">
-			<div class="white mr-2 rounded-circle pa-1">
-				<v-img
-					class="shrink "
-					contain
-					src="https://images.squarespace-cdn.com/content/600fff8831539d708a565cc6/1611667752413-J16I28OW25P5CRTIA2OO/dreamcatcher.png?content-type=image%2Fpng"
-					transition="scale-transition"
-					width="40"
-				/>
-			</div>
-			Dreamlix
-		</v-card-title>
-		<v-card-text>Made with <v-icon small color="red">mdi-heart</v-icon> in South Africa</v-card-text>
 		<v-card-actions class="justify-space-between px-5">
 			<v-btn icon><v-icon>mdi-facebook</v-icon></v-btn>
 			<v-btn icon><v-icon>mdi-instagram</v-icon></v-btn>
 			<v-btn icon><v-icon>mdi-twitter</v-icon></v-btn>
-			<v-btn icon><v-icon>mdi-github</v-icon></v-btn>
 		</v-card-actions>
 	</v-card>
 </template>
